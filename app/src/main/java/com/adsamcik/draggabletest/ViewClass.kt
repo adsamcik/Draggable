921cd53e427fc99f5d6c4fb639ffa4e934b794e1
adsamcik <adsamcik@users.noreply.github.com>
2018-02-09 20:44:35 +0100

    Updated view class

--- app/src/main/java/com/adsamcik/draggabletest/ViewClass.kt
+++ app/src/main/java/com/adsamcik/draggabletest/ViewClass.kt
@@ -1,32 +1,29 @@
 package com.adsamcik.draggabletest
 
+import android.app.Activity
 import android.app.Fragment
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.adsamcik.draggable.IOnDemandView
 
 class ViewClass : Fragment(), IOnDemandView {
+    override fun onPermissionResponse(requestCode: Int, success: Boolean) {
+        Log.d("ViewClass", "Permission request")
+    }
+
+    override fun onEnter(activity: Activity) {
+        Log.d("ViewClass", "Enter")
+    }
+
+    override fun onLeave(activity: Activity) {
+        Log.d("ViewClass", "Exit")
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val view = inflater!!.inflate(R.layout.fragment_view_class, container, false)
-        return view
-    }
-    override fun onEnter(activity: FragmentActivity) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onLeave(activity: FragmentActivity) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onPermissionResponse(requestCode: Int, success: Boolean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onHomeAction() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return inflater!!.inflate(R.layout.fragment_view_class, container, false)
     }
 }
