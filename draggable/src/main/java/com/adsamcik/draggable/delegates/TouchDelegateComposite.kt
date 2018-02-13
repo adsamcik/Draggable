733a8f9c4f579e43cb505e2f0bf8d2eb9d19de07
adsamcik <adsamcik@users.noreply.github.com>
2018-02-13 10:18:11 +0100

    Add javadoc

--- draggable/src/main/java/com/adsamcik/draggable/delegates/TouchDelegateComposite.kt
+++ draggable/src/main/java/com/adsamcik/draggable/delegates/TouchDelegateComposite.kt
@@ -5,15 +5,28 @@ import android.view.MotionEvent
 import android.view.TouchDelegate
 import android.view.View
 
-
+/**
+ * Touch delegate composite is a implementation of TouchDelegate
+ * that allows multiple TouchDelegates per view
+ */
 internal class TouchDelegateComposite(view: View) : TouchDelegate(emptyRect, view) {
     private val delegates = ArrayList<AbstractTouchDelegate>()
     val count: Int get() = delegates.size
 
+    /**
+     * Add delegate to the composite
+     *
+     * @param delegate Abstract touch delegate to add
+     */
     fun addDelegate(delegate: AbstractTouchDelegate) {
         delegates.add(delegate)
     }
 
+    /**
+     * Removes delegate from the composite
+     *
+     * @param delegate Abstract touch delegate to remove
+     */
     fun removeDelegate(delegate: AbstractTouchDelegate) {
         delegates.remove(delegate)
     }
@@ -22,7 +35,7 @@ internal class TouchDelegateComposite(view: View) : TouchDelegate(emptyRect, vie
         val x = event.x
         val y = event.y
 
-        delegates.sortByDescending { it.view.translationZ}
+        delegates.sortByDescending { it.view.translationZ }
         delegates.forEach {
             event.setLocation(x, y)
             if (it.onTouchEvent(event))
@@ -35,12 +48,27 @@ internal class TouchDelegateComposite(view: View) : TouchDelegate(emptyRect, vie
     companion object {
         val emptyRect = Rect()
 
+        /**
+         * Automatically adds touch delegate to view
+         *
+         * If touch delegate already exists and is not TouchDelegateComposite it will be
+         * replaced with TouchDelegateComposite and added to it so it will still be called
+         * Unfortunately there is no way to determine the view it handles so passed view's
+         * Z translation will be used for sorting
+         */
         fun addTouchDelegateOn(view: View, delegate: AbstractTouchDelegate): TouchDelegateComposite {
             val composite = addTouchDelegateOn(view)
             composite.addDelegate(delegate)
             return composite
         }
 
+        /**
+         * Automatically creates TouchDelegateComposite on the view
+         *
+         * If touch delegate already exists and is not TouchDelegateComposite it will be
+         * replaced with TouchDelegateComposite and added to it so it will still be called
+         * Unfortunately there is no way to determine the view it focuses on so it will parent view for sorting
+         */
         fun addTouchDelegateOn(view: View): TouchDelegateComposite {
             val delegate = view.touchDelegate
 
@@ -49,7 +77,7 @@ internal class TouchDelegateComposite(view: View) : TouchDelegate(emptyRect, vie
 
             val composite = TouchDelegateComposite(view)
             if (delegate != null)
-                composite.addDelegate(WrapperTouchDelegate(view,    delegate))
+                composite.addDelegate(WrapperTouchDelegate(view, delegate))
 
             view.touchDelegate = composite
             return composite
--- draggable/src/main/java/com/adsamcik/draggable/delegates/WrapperTouchDelegate.kt
+++ draggable/src/main/java/com/adsamcik/draggable/delegates/WrapperTouchDelegate.kt
@@ -4,6 +4,10 @@ import android.view.MotionEvent
 import android.view.TouchDelegate
 import android.view.View
 
-class WrapperTouchDelegate(override val view: View, private val delegate: TouchDelegate) : AbstractTouchDelegate() {
+/**
+ * WrapperTouchDelegate is wrapper for the standard TouchDelegate
+ * so it can be used in TouchDelegateComposite
+ */
+internal class WrapperTouchDelegate(override val view: View, private val delegate: TouchDelegate) : AbstractTouchDelegate() {
     override fun onTouchEvent(event: MotionEvent): Boolean = delegate.onTouchEvent(event)
 }
